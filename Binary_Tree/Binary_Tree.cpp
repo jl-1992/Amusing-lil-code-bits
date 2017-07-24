c80034bdc3df5c7aa210297805dadd991e708df8
Joseph L <joseph.earl.ludy@gmail.com>
2017-07-24 09:07:16 -0700

    Update Binary_Tree.cpp

--- Binary_Tree/Binary_Tree.cpp
+++ Binary_Tree/Binary_Tree.cpp
@@ -42,12 +42,11 @@ int main()
 	Node * c = new Node(nullptr,nullptr,4);
 	Node * d = new Node(nullptr,nullptr,23);
 	Node * e = new Node(a,nullptr,14);
-	Node * f = new Node(nullptr,nullptr,102);
 	Node * g = new Node(b,c,103);
 	Node * h = new Node(nullptr,nullptr,-3);
 	Node * i = new Node(d,e,6);
 	Node * j = new Node(nullptr,h,71);
-	Node * l = new Node(f,g,11);
+	Node * l = new Node(h,g,11);
 	Node * m = new Node(i,j,3);
 	Node * n = new Node(j,l,5);
 	Node * o = new Node(m,n,2);
